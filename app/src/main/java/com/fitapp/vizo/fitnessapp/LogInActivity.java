febc97468164a16e871af4f74f1d812c073ef58c
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-11-18 11:10:14 -0800

    sign-up user form submit

--- app/src/main/java/com/fitapp/vizo/fitnessapp/LogInActivity.java
+++ app/src/main/java/com/fitapp/vizo/fitnessapp/LogInActivity.java
@@ -1,13 +1,34 @@
 package com.fitapp.vizo.fitnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LogInActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        ButterKnife.bind(this);
+
+    }
+
+    public void onClickUserSignUp(View v) {
+        Intent signUpIntent = new Intent(LogInActivity.this, SignUpActivity.class);
+        startActivity(signUpIntent);
+    }
+
+    public void onClickUserLogin(View v) {
+
     }
 }
--- app/src/main/java/com/fitapp/vizo/fitnessapp/SignUpActivity.java
+++ app/src/main/java/com/fitapp/vizo/fitnessapp/SignUpActivity.java
@@ -2,12 +2,43 @@ package com.fitapp.vizo.fitnessapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+    @Bind(R.id.newUserFirstName) EditText newUserFirstNameField;
+    @Bind(R.id.newUserLastName) EditText newUserLastNameField;
+    @Bind(R.id.newUserUsername) EditText newUserUsernameField;
+    @Bind(R.id.newUserPassword) EditText newUserPasswordField;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ButterKnife.bind(this);
     }
+
+    public void onClickSubmitUserContact(View v) {
+        String newUserFirstNameInput = newUserFirstNameField.getText().toString();
+        String newUserLastNameInput = newUserLastNameField.getText().toString();
+        String newUserUsernameInput = newUserUsernameField.getText().toString();
+        String newUserPasswordInput = newUserPasswordField.getText().toString();
+        Log.d("test",newUserFirstNameInput);
+        Log.d("test",newUserLastNameInput);
+        Log.d("test",newUserUsernameInput);
+        Log.d("test",newUserPasswordInput);
+        Toast.makeText(SignUpActivity.this, "Hmmmm", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
