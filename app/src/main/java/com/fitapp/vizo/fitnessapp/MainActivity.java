c870b6c36556f1f80b9bc9c9f74adf3ee0ac95d0
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-11-18 17:19:36 -0800

    added array list to display sample api call of exercises

--- app/src/main/java/com/fitapp/vizo/fitnessapp/MainActivity.java
+++ app/src/main/java/com/fitapp/vizo/fitnessapp/MainActivity.java
@@ -3,7 +3,13 @@ package com.fitapp.vizo.fitnessapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.io.Serializable;
 
 import butterknife.Bind;
@@ -11,8 +17,15 @@ import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.muscleGroupSelected) TextView muscleGroupSelected;
+    @Bind(R.id.exerciseList)
+    ListView mListView;
     @Bind(R.id.userFirstName) TextView userFirstName;
     User currentUser;
+
+    private String[] exercises = new String[] {"Incline Hammer Curls", "Incline Inner-Biceps Curl", "Standing Concentration Curl", "EZ Bar Curl",
+            "Wide-grip standing barbell curl", "Barbell Curl", "Dumbbell Biceps Curl"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
         Intent receiveUser = getIntent();
         currentUser = (User) receiveUser.getSerializableExtra("userSelected");
         userFirstName.setText(currentUser.getFirstName());
+
+
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, exercises);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String exercise = ((TextView)view).getText().toString();
+                Toast.makeText(MainActivity.this, exercise, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
