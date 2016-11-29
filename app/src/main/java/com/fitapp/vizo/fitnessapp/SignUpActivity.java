e602c7e120c6248208f8a36a58199fb80903632d
benviss <ben.vissotzky@gmail.com>
2016-11-29 14:55:20 -0800

    update formatting

--- app/src/main/java/com/fitapp/vizo/fitnessapp/SignUpActivity.java
+++ app/src/main/java/com/fitapp/vizo/fitnessapp/SignUpActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
